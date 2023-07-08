fe5f1bb41f124a347c058841932c437cb807afb2
Harsh Sharma <harsh.ju.sharma@gmail.com>
2023-07-08 10:34:02 +0530

    Setup Fixes

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      HomePage Check
+      <button
+        onClick={() => {
+          navigate("/editor");
+        }}
+      >
+        Editor
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
