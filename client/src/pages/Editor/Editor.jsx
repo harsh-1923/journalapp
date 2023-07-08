9de4722ddcc81931de920b0014704eb26ed4480c
Harsh Sharma <harsh.ju.sharma@gmail.com>
2023-07-08 10:13:15 +0530

    Speech Recognition Setup

--- client/src/pages/Editor/Editor.jsx
+++ client/src/pages/Editor/Editor.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div>
+      <p>Microphone: {listening ? "on" : "off"}</p>
+      {/* <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button> */}
+
+      <button
+        onMouseDown={SpeechRecognition.startListening}
+        onMouseUp={SpeechRecognition.stopListening}
+      >
+        Hold to speak
+      </button>
+      <p>{transcript}</p>
+    </div>
+  );
 };
 
 export default Editor;
